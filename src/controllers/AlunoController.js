ca63885ed6064d8bc2c1074d8991092e32a8bf52
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-03-27 15:35:23 -0300

    Rotas de alunos

---
+++ src/controllers/AlunoController.js
@@ -0,0 +1,11 @@
+import Aluno from '../models/Aluno';
+
+class AlunoController {
+  async index(req, res) {
+    const aluno = await Aluno.findAll();
+
+    res.json(aluno);
+  }
+}
+
+export default new AlunoController();
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,17 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'edsu',
-      sobrenome: 'da Silva',
-      email: 'edsu@aluno.com',
-      idade: 21,
-      peso: 90,
-      altura: 1.80,
-    });
-
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
