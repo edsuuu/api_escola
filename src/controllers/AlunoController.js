f224d68a7058bc135cae85da5a5c8b7a8684bf51
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-03-31 14:15:49 -0300

    Crud de Alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -6,6 +6,87 @@ class AlunoController {
 
     res.json(aluno);
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          error: ['Id não informado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          error: ['Aluno não existe'],
+        });
+      }
+
+      return res.json(aluno); // retorna o aluno encontrado.
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          error: ['Id não informado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          error: ['Aluno não existe'],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body); // atualiza o aluno.
+
+      return res.json(alunoAtualizado); // retorna o aluno encontrado.
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          error: ['Id não informado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          error: ['Aluno não existe'],
+        });
+      }
+
+      await aluno.destroy(); // deleta o aluno.
+      return res.json('Aluno apagado');
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new AlunoController();
